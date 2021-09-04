93b1da7b599f15c3cbcc9f718d931b9b28d3815a
Hrishikesh <hrishikeshps@gmail.com>
2021-09-04 17:21:58 +0530

    Adding logic to close sidebar after the route has been changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Router, NavigationEnd, Event as NavigationEvent } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
+import { filter } from 'rxjs/operators';
+
+import { NavbarComponent } from './shared/layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'matrix-machine-angular';
+
+  @ViewChild(NavbarComponent) navbar!: NavbarComponent;
+
+  constructor(private router: Router, private element: ElementRef) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEvent) => {
+        if (!this.navbar) return;
+        this.navbar.toggleNav();
+      });
+  }
 }
